df51f022cac12773b8c256fd0c9a016c999e8a96
AleksandraNurgalieva <pavlukevichalexandra@gmail.com>
2024-11-11 01:22:45 +0400

    Revert "lesson4"

--- Park.java
+++
@@ -1,8 +0,0 @@
-public class Park {
-    public class Attraction {
-        private String name;
-        private String description;
-        private String worktime;
-        private  int cost;
-    }
-}
