c40569db004aeb84e4f44afbc8f2541478899a69
Alexandra <pavlukevichalexandra@gmail.com>
2024-11-11 01:33:04 +0400

    lesson4-1

---
+++ Park.java
@@ -0,0 +1,8 @@
+public class Park {
+    public class Attraction {
+        private String name;
+        private String description;
+        private String worktime;
+        private  int cost;
+    }
+}
